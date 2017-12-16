df49f088ba23fd97f4bb82576bf1f52e5c77725f
Kerin Khan <kerink585@gmail.com>
2017-12-15 18:04:27 -0800

    need to change back color of squares to original color

--- simonGame.js
+++ simonGame.js
@@ -1,10 +1,10 @@
 var simonTurn=false;
 var simonArr=[];
 var userArr=[];
-var userTurn =false;
+var userTurn =true;
 var MaxSimonSays=20;
-var count=0;
 var correct=0;
+var myvar;
 
 
 function start(){
@@ -23,74 +23,61 @@ function stop(){
 
 function simonSays(){
 	/*this function will mimic simon*/
-	var myvar;
+	userArr=[];
+	correct=0;
+	
+	console.log('userarr In simon func',userArr)
 	if(simonTurn){
 		var simonInput = Math.floor(Math.random()*4);
 		simonArr.push(simonInput);
-		console.log(simonArr);
+		console.log('simon',simonArr);
 		for(var i=0;i<simonArr.length;i++){
-			console.log('i',i);
 			var num2Str=simonArr[i].toString();
-			console.log(num2Str);
 			if(simonArr[i]===0){
 				var element= document.getElementsByClassName('btn'+num2Str)[0];
 				var originColr= element.style.backgroundColor;
-				element.style.backgroundColor='#00FF00'
+				element.style.backgroundColor='#88DA43'
 				myvar=setTimeout(function(){
 			
 					
 					element.style.backgroundColor=originColr;//green
-					console.log("inside settimer 0");
 					clearTimeout(myvar);
 
 				}, 1000);
 				
-				console.log("out settimer 0");
 			}
 			else if(simonArr[i]==1){
 				var element= document.getElementsByClassName('btn'+num2Str)[0];
 				var originColr= element.style.backgroundColor;
-				element.style.backgroundColor='#FF004C'
+				element.style.backgroundColor='#962200'
 				myvar=setTimeout(function(){
-					
-					
 					element.style.backgroundColor=originColr; //red
-					console.log("inside settimer 1");
-					
 				}, 1000);
 				
-				console.log("out settimer 1");
-
-
 			}
 			else if(simonArr[i]==2){
 				var element= document.getElementsByClassName('btn'+num2Str)[0];
 				var originColr= element.style.backgroundColor;
-				element.style.backgroundColor='#EBFF74'
+				element.style.backgroundColor='#EBFFC5'
 				
-				myvar=setTimeout(function(){
-				
+				myvar=setTimeout(function(){				
 					element.style.backgroundColor=originColr;//yellow
-					console.log("inside settimer 2");
 					clearTimeout(myvar);
 				}, 1000);
-				
-				console.log("out settimer 2");
 
 			}
 			else if(simonArr[i]==3){
 				var element= document.getElementsByClassName('btn'+num2Str)[0];
 				var originColr= element.style.backgroundColor;
-				element.style.backgroundColor='#0065FF'
+				element.style.backgroundColor='#00C4FF';
 				myvar= setTimeout(function(){
 					element.style.backgroundColor=originColr;//blue
-					console.log("inside settimer 3");
+					clearTimeout(myvar);
 					clearTimeout(myvar);
 				}, 1000);
-				console.log("out settimer 3");
-
 			}
 			else{
+				clearTimeout(myvar);
 				console.log("oops, something is wrong");
 			}
 			//clearTimeout(myvar);
@@ -99,15 +86,17 @@ function simonSays(){
 
 	}
 
-	userTurn=true;
 	console.log("userTurn");
 	//userMemory();
 }
 
 
 function userMemory(btn){
+	
 	if(userTurn){
+		simonTurn=false;
 		var userInput = btn.value;
+		//console.log(btn);
 		var str2num=Number(userInput);
 		console.log('userArr',userArr);
 		userArr.push(str2num);
@@ -115,16 +104,21 @@ function userMemory(btn){
 		if(userArr.length===simonArr.length){
 			check();
 
+			
+
 		}
+		
 
 
 	}
-
-
 }
 
 
 function check(){
+
+	var count= simonArr.length;
+	document.getElementById('count').innerHTML=count;
+
 	console.log('user',userArr);
 	console.log('in check()');
     for (var i=0; i<userArr.length;i++){
@@ -133,14 +127,15 @@ function check(){
     		console.log('in check() condi');
     		correct++;
     		console.log('correct',correct);
-    		count++;
+    		count--;
     		console.log('count',count);
+    		document.getElementById('count').innerHTML=count;
     		console.log('userArrlength',userArr.length);
     		console.log('user',userArr);
     		if(correct===simonArr.length){
-    			userTurn=false;
     			simonTurn=true;
     			simonSays();
+    			
     		}
     	}
     	else if (simonArr[i]!==userArr[i]){
@@ -148,9 +143,9 @@ function check(){
     		console.log('no match');
     		userTurn=false;
     		simonTurn=false;
-    		simonSays
     	}
     }
-    userArr=[];
+    
+    
 }
 
