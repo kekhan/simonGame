dc9f9389c4656aa67e966146e65d864374574c9f
Kerin Khan <kerink585@gmail.com>
2018-06-24 23:04:38 -0700

    updates

--- simonGame.js
+++ simonGame.js
@@ -21,7 +21,6 @@ function simonTurn(){
 		// call the randomColor generater
 		getRandomNumber();
 		console.log(queuedNumbers);
-
 		for(var i=0; i<queuedNumbers.length;i++){
 			if(queuedNumbers[i] == 0){
 				// change the color of the box
@@ -30,16 +29,9 @@ function simonTurn(){
 				element = document.getElementsByClassName(idPlaceHolder)[0];
 				var color = '#31E644';
 				element.style.backgroundColor = color;
-
-
 				setInterval(function(){
-
 					element.style.backgroundColor = COLORARRAY[3];
-
 				},1000);
-
-
-
 			}
 			else if(queuedNumbers[i] == 1){
 								// change the color of the box
@@ -48,15 +40,9 @@ function simonTurn(){
 				element = document.getElementsByClassName(idPlaceHolder)[0];
 				var color = '#DF3140';
 				element.style.backgroundColor = color;
-
-
 				setInterval(function(){
-
 					element.style.backgroundColor = COLORARRAY[2];
-
 				},1000);
-
-
 			}
 			else if(queuedNumbers[i] == 2){
 				var numToString = queuedNumbers[i].toString();
@@ -64,15 +50,9 @@ function simonTurn(){
 				element = document.getElementsByClassName(idPlaceHolder)[0];
 				var color = '#fffb21';
 				element.style.backgroundColor = 'color';
-
-
 				setInterval(function(){
-
 					element.style.backgroundColor = COLORARRAY[1];
-
 				},1000);
-
-
 			}
 			else if(queuedNumbers[i] == 3){
 				var numToString = queuedNumbers[i].toString();
@@ -81,31 +61,22 @@ function simonTurn(){
 
 				var color = '#5496BF';
 				element.style.backgroundColor = color;
-
-
 				setInterval(function(){
-
 					element.style.backgroundColor = COLORARRAY[0];
-
 				},1000);
-
-
 			}
 		}
 		compTurn = false;
 		userTurn = true;
 		user_input();
 	}
-
 }
 
 function getRandomNumber(){
 	// produces a number 0-3 for index of color array
 	var number = Math.floor(Math.random()*4);
 	queuedNumbers.push(number);
-
 }
-
 function checkMatch(){
 	// checks if clicks match simon's array
 	console.log("checking");
@@ -132,13 +103,9 @@ function changeColor(element,color){
 	this function will change the color of a square
 	*/
 	element.style.backgroundColor = color;
-
-
 	setInterval(function(){
-
 		console.log(element,color);
 	},500);
-
 }
 
 //start the game
